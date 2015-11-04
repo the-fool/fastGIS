46e30f9f4358c72ebf182fbe92a81b90222c16a0
Thomas Ruble <sketchbang@gmail.com>
2015-11-04 11:24:41 -0600

    initial commit

---
+++ mpikrig.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <string.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <errno.h>
+
+int slave(int rank, int comm_sz);
+
+int main(int argc, char **argv) 
+{
+  int rank, comm_sz, r_status;
+  
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    
+  if (rank != 0) {
+    printf("Process %d out of %d processes in executing.\n", rank, comm_sz);
+    r_status = slave(rank, comm_sz); 
+  }
+ 
+  else {
+    printf("Master process reporting.\n", rank, comm_sz);
+    // master(comm_sz);   // to implement
+    r_status = slave(rank, comm_sz);
+  }
+
+  printf("Process %d reports status: %d\n", rank, r_status);
+  MPI_Finalize();
+  return errno;
+}
+
+int slave(int rank, int comm_sz) {
+  return system("nohup R < ana3.r &");
+}
+
+  
