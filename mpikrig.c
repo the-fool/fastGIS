c447aefda7d70ee8e2a6d24c93ef7f4c9452c041
Thomas Ruble <sketchbang@gmail.com>
2015-11-04 15:44:09 -0600

    add save obj id

--- mpikrig.c
+++ mpikrig.c
@@ -15,7 +15,6 @@ int main(int argc, char **argv)
   MPI_Init(NULL, NULL);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-    
   if (rank != 0) {
     printf("Process %d out of %d processes executing.\n", rank, comm_sz);
     r_status = slave(rank, comm_sz); 
@@ -26,15 +25,22 @@ int main(int argc, char **argv)
     // master(comm_sz);   // to implement
     r_status = slave(rank, comm_sz);
   }
-
+ 
   printf("Process %d reports status: %d\n", rank, r_status);
-  MPI_Finalize();
-  return errno;
+  if (errno)
+    perror("pre-finalize");
+  if (MPI_Finalize() != 0) {
+      perror("?");
+  }
+  
+  return 0;
 }
 
 int slave(int rank, int comm_sz) { 
-  char exec[32];
-  sprintf(exec, "R < ana3.R > log%d.txt 2>&1 --no-save", rank);
+  static int i = 0;
+  i++;
+  char exec[64];
+  sprintf(exec, "Rscript ana3.R krig_r%d_%d.rda > log%d.txt 2>&1", rank, i, rank);
   return system(exec);
 }
 
