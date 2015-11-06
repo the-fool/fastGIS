4a1e460dee9f7141b8c0d99370adf13f7dc74a7f
Thomas Ruble <sketchbang@gmail.com>
2015-11-05 20:02:22 -0600

    modify logging logic

--- mpikrig.c
+++ mpikrig.c
@@ -5,43 +5,60 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <errno.h>
+#include <time.h>
 
 int slave(int rank, int comm_sz);
+char *timestamp();
 
 int main(int argc, char **argv) 
 {
   int rank, comm_sz, r_status;
   
-  MPI_Init(NULL, NULL);
+  MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-  if (rank != 0) {
-    printf("Process %d out of %d processes executing.\n", rank, comm_sz);
-    r_status = slave(rank, comm_sz); 
+  
+  // MPI_Errhandlerset(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: krig n_iter\n");
+    exit(1);
   }
- 
-  else {
-    printf("Master process reporting.\n", rank, comm_sz);
-    // master(comm_sz);   // to implement
-    r_status = slave(rank, comm_sz);
+  int n = atoi(argv[1]);
+  printf("Begin %d iterations of %d processes\n", n, comm_sz);
+  int i;
+  for (i = 0; i < n; i++) {   
+    slave(rank, comm_sz);
   }
- 
-  printf("Process %d reports status: %d\n", rank, r_status);
+  
   if (errno)
     perror("pre-finalize");
   if (MPI_Finalize() != 0) {
-      perror("?");
+      perror("MPI_Finalize");
   }
-  
+
   return 0;
 }
 
 int slave(int rank, int comm_sz) { 
   static int i = 0;
-  i++;
+  int r_status;
   char exec[64];
-  sprintf(exec, "Rscript ana3.R krig_r%d_%d.rda > log%d.txt 2>&1", rank, i, rank);
-  return system(exec);
+  char *tstamp = timestamp();
+  
+  i++;
+  printf("%sProc %d on iteration: %d\n\n", tstamp, rank, i);
+  sprintf(exec, "Rscript Ana2.R krig_r%d_%d.rda >> log%d.txt 2>&1", rank, i, rank);
+  if ((r_status = system(exec)) != 0)
+    printf("ERROR: Proc %d reports status: %d\n", rank, r_status);
+  return r_status;
 }
 
-  
+char *timestamp()
+{
+  static char tstamp[128];
+  time_t ltime;
+  ltime=time(NULL);
+  sprintf(tstamp, "%s", asctime( localtime(&ltime) ) );
+  return tstamp;
+}
