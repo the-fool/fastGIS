e424c4e114dfd9043c8291e7e3d71adc24eb5013
Thomas Ruble <sketchbang@gmail.com>
2015-11-08 22:42:14 -0600

    more celery testing

--- app/main/views.py
+++ app/main/views.py
@@ -2,11 +2,22 @@ from . import main
 
 from ..tasks import rtest
 
+result = None
+
 @main.route('/')
 def index():
     return "I work"
 
 @main.route('/celtest')
 def celtest():
+    global result 
     result = rtest.delay()
     return result.state
+
+@main.route('/celtest2')
+def celtest2():
+    global result
+    if result is not None:
+        return result.state
+    else:
+        return "nothing"
